780a17d452dc465c9eb843999d09252c69ceb73d
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-12-14 19:20:08 -0600

    adding navbar image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react";
+import "./Navbar.css";
 import { Navbar } from "react-bootstrap";
 let styles = {
   navbar: {
     color: "white",
+    marginLeft: "1em",
+    fontSize: "3em",
   },
 };
 
 const Navbar1 = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar className="navbar" bg="dark" variant="dark">
       <h2 style={styles.navbar}>Daniel Sosa</h2>
       <Navbar.Brand href="#home"></Navbar.Brand>
     </Navbar>
