039d0da81b604a4945f9e0e16649731df00e20a4
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2022-01-04 20:55:30 -0600

    updates projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,22 +20,13 @@ function ProjectCards(props) {
       </div>
       <div className="content">
         <p className="card-title">{props.name}</p>
-        <hr></hr>
         <div className="overlay">
           <p class="proj-icons-container">
             <a href={props.github}>
-              <img
-                class="project-icon"
-                src="https://img.icons8.com/fluent/48/000000/github.png"
-                alt="GitHub Repo"
-              />
+              <button className="project-icon">Github</button>
             </a>
             <a href={props.deploy}>
-              <img
-                class="project-icon"
-                src="https://img.icons8.com/fluent/48/000000/domain.png"
-                alt="Live Application"
-              />
+              <button class="project-icon">Live Application</button>
             </a>
           </p>
           <p class="topics">{props.topics}</p>
